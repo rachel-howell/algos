0e2fccbb668de69f538458940d25040c1347a60d
rachel-howell <rhowell96@gmail.com>
2024-02-22 17:36:14 -0800

    Solve maxmimum consecutive ones/binary flip problem

--- max_consecutive_ones.js
+++ max_consecutive_ones.js
@@ -52,27 +52,62 @@
 //once you reach a point where the subarray contains >k zeroes, we must make a while loop to take items off the left of the subarray until it meets the criterion for validity again
 
 //third try oon 2.21.24
-var longestOnes = function(nums, k) {
-  console.log("hi")
-  let left = 0, ans = 0, numZeros = 0;
-  //left will increment in the while loop that is active when the number of zeroes is greater than k
-    //ans keeps track of the greatest consecutive num of 1s with k 0 flips
-    //numZeros keeps track of how many zeroes there are
-  for(let right=0; right<nums.length; right++) {
+// var longestOnes = function(nums, k) {
+//   console.log("hi")
+//   let left = 0, ans = 0, numZeros = 0;
+//   //left will increment in the while loop that is active when the number of zeroes is greater than k
+//     //ans keeps track of the greatest consecutive num of 1s with k 0 flips
+//     //numZeros keeps track of how many zeroes there are
+//   for(let right=0; right<nums.length; right++) {
       
-    if(nums[right] == 0) {
-        numZeros++;
-    }
-    while(numZeros > k) {
-        if(nums[right] == 0) {
-            numZeros--;
-        }
-        left++;
-    }
+//     if(nums[right] == 0) {
+//         numZeros++;
+//     }
+//     while(numZeros > k) {
+//         if(nums[right] == 0) {
+//             numZeros--;
+//         }
+//         left++;
+//     }
       
-    ans = Math.max(ans, right - left + 1);
+//     ans = Math.max(ans, right - left + 1);
 
-  }
+//   }
   
-  return ans;
+//   return ans;
+// };
+
+// fourth time's the charm!!
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var longestOnes = function(nums, k) {
+for(let right=0; right<nums.length; right++) {
+      
+  if(nums[right] == 0) {
+      console.log("that was a zero!!")
+      numZeros++;
+  }
+    
+  console.log("The array at position " + right + "has a value of " + nums[right] + ". The number of zeros right now is" + numZeros + ".")
+    
+  while(numZeros > k) {
+      console.log("The while loop is running now. We are at " + numZeros + " zeros, and the current streak is " + (right - left + 1))
+      if(nums[left] == 0) {
+          numZeros--;
+      }
+      left++;
+      console.log("WHILE LOOP JUST RAN! we are at " + numZeros + " zeros. The variable left has value " + left)
+  }
+    
+  ans = Math.max(ans, right - left + 1);
+
+}
+
+return ans;
 };
+
+// SUMMARY:
+// In the binary flip problem, you have to use the number of zeroes in the current subarray as a constraint. So first you need to set up a for loop to iterate through the array. Then, make a conditional statement that checks to see if the current integer is 0, and if it is, increment a variable used to keep track of how many zeroes there are. Underneath the conditional statement, set up a while loop that takes zeroes off the left side of the subarray until it is a valid subarray again by moving up the var "left" which tracks the left side of the sliding window. Then figure out if the current subarray is larger than the previous maximum.
