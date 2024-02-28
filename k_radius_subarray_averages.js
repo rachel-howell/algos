fb6298cff4253c5e9c438c43c81db91c6708e0e6
rachel-howell <rhowell96@gmail.com>
2024-02-27 16:49:14 -0800

    Analyze solution

--- k_radius_subarray_averages.js
+++ k_radius_subarray_averages.js
@@ -34,3 +34,37 @@ if (arr[5] == undefined) {
 // to find k-radius at index 3 when k = 2...
 // avg = 25 / 5 = 5
 
+//Solution:
+var getAverages = function(nums, k) {
+  // When a single element is considered then its average will be the number itself only.
+  if (k === 0) {
+      return nums;
+  }
+  
+  const windowSize = 2 * k + 1
+  const n = nums.length;
+  const averages = new Array(n).fill(-1);
+
+  // Any index will not have 'k' elements in its left and right.
+  if (windowSize > n) {
+      return averages;
+  }
+
+  // Generate 'prefix' array for 'nums'.
+  // 'prefix[i + 1]' will be sum of all elements of 'nums' from index '0' to 'i'.
+  const prefix = new Array(n + 1).fill(0);
+  for (let i = 0; i < n; ++i) {
+      prefix[i + 1] = prefix[i] + nums[i];
+  }
+  
+  // We iterate only on those indices which have at least 'k' elements in their left and right.
+  // i.e. indices from 'k' to 'n - k'
+  for (let i = k; i < (n - k); ++i) {
+      const leftBound = i - k, rightBound = i + k;
+      const subArraySum = prefix[rightBound + 1] - prefix[leftBound];
+      const average = Math.floor(subArraySum / windowSize);
+      averages[i] = average;
+  }
+
+  return averages;
+};
