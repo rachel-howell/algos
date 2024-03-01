8f392ca9d967820f5236ca90776528819afbf7b0
rachel-howell <rhowell96@gmail.com>
2024-02-29 18:16:51 -0800

    Solve k-radius algo finally

--- k_radius_subarray_averages.js
+++ k_radius_subarray_averages.js
@@ -4,11 +4,7 @@
  * @return {number[]}
  */
 
-
-
-
-//Solution:
-var getAveragesRachel = function(nums, k) {
+var getAverages = function(nums, k) {
 
   if(k==0) {
     return nums;
@@ -25,17 +21,19 @@ var getAveragesRachel = function(nums, k) {
   }
 
   // making prefix sum array:
-  let prefix = new Array(n + 1).fill(0)
-  for(let i = 0; i < n; i++) {
-    // ========================================+> FIX THIS
-    // prefix.push(prefix[i - 1] + nums[i]);
+  // let prefix = new Array(n + 1).fill(0)
+  // console.log("Rachel's initial prefix array is " + prefix)
+  let prefix = [nums[0]]
+  for(let i = 1; i < n; i++) {
+    prefix.push(prefix[i - 1] + nums[i]);
   }
   console.log("Rachel's prefix sum array is " + prefix);
 
   for(let i=k; i < (n - k); i++) {
     let leftBound = i - k;
     let rightBound = i + k;
-    let subarraySum = prefix[rightBound + 1] - prefix[leftBound];
+    let subarraySum = prefix[rightBound] - prefix[leftBound] + nums[leftBound];
+    // ^^ line 39 was my issue, I wasn't calculating the subarraySum properly. We take the number at the right of the window of the prefix sum (which gives us the sum of the elements up to that point) and subtract the numbers to the left of the leftbound. We add the nums[leftBound] element because when we do the subtraction, we forget to include the number at the start of our subarray (the left end). So, we need to add that number back in.
     let average = Math.floor(subarraySum/window)
     averages[i] = average;
   }
@@ -45,40 +43,7 @@ var getAveragesRachel = function(nums, k) {
 
 };
 
-var getAverages = function(nums, k) {
-  // When a single element is considered then its average will be the number itself only.
-  if (k === 0) {
-      return nums;
-  }
-  
-  const windowSize = 2 * k + 1
-  const n = nums.length;
-  const averages = new Array(n).fill(-1);
 
-  // Any index will not have 'k' elements in its left and right.
-  if (windowSize > n) {
-      return averages;
-  }
-
-  // Generate 'prefix' array for 'nums'.
-  // 'prefix[i + 1]' will be sum of all elements of 'nums' from index '0' to 'i'.
-  const prefix = new Array(n + 1).fill(0);
-  for (let i = 0; i < n; ++i) {
-      prefix[i + 1] = prefix[i] + nums[i];
-  }
-  console.log("The other prefix array is: " + prefix);
-  
-  // We iterate only on those indices which have at least 'k' elements in their left and right.
-  // i.e. indices from 'k' to 'n - k'
-  for (let i = k; i < (n - k); ++i) {
-      const leftBound = i - k, rightBound = i + k;
-      const subArraySum = prefix[rightBound + 1] - prefix[leftBound];
-      const average = Math.floor(subArraySum / windowSize);
-      averages[i] = average;
-  }
-
-  return averages;
-};
 
 console.log(getAverages([1,2,3,4,5], 1))
-console.log(getAveragesRachel([1,2,3,4,5], 1))
+
