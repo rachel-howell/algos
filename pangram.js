4672f578847cf49d9201d5718bdee7d5bf858087
rachel-howell <rhowell96@gmail.com>
2024-03-08 16:30:46 -0800

    Finish pangram algo using hashmap

--- pangram.js
+++ pangram.js
@@ -4,13 +4,18 @@
  */
 var checkIfPangram = function(sentence) {
   let dic = new Map();
-  console.log("hi")
   for(let i=0; i<sentence.length; i++) {
       let letter = sentence[i];
       if(dic.has(letter)) {
-        
+        continue;
       }
+      dic.set(letter, i);
+  }
+  if(dic.size == 26) {
+    return true;
+  } else {
+    return false;
   }
 };
 
-checkIfPangram("thequickbrownfoxjumpsoverthelazydog");
+console.log(checkIfPangram("theuickbrownfoxjumpsoverthelazydog"))
