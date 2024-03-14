b1aaa0ade708064fe3f31c34a1df1ee1b7ad124c
rachel-howell <rhowell96@gmail.com>
2024-03-14 16:34:11 -0700

    Solve counting elements hashmap problem

--- counting_elements.js
+++ counting_elements.js
@@ -5,5 +5,27 @@
  * @return {number}
  */
 var countElements = function(arr) {
-    
+  // make a map that contains all elements of the arr
+  // initialize var to keep track of how many elements match criteria
+  // make a for loop that looks at all elements of arr and has several conditionals:
+    // if nums[i + 1] == nums[i] + 1
+      // if map.has(nums[i]) => total++
+      // if !map.has(nums[i]) => map.set(nums[i], i)
+    // else continue
+
+let map = new Map();
+let total = 0;
+
+for(let i=0; i < arr.length; i++) {
+  map.set(arr[i], i);
+}
+
+for(let i=0; i < arr.length; i++) {
+  if(map.has(arr[i] + 1)) {
+    total++;
+  }
+}
+  
+return total;
+
 };
