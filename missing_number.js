1816892cb114e5ee9607b199bc410a793a6de5f9
rachel-howell <rhowell96@gmail.com>
2024-03-13 17:53:48 -0700

    Keep working on missing number problem

--- missing_number.js
+++ missing_number.js
@@ -5,20 +5,28 @@
 var missingNumber = function(nums) {
   let map = new Map();
   nums.sort();
-
-  
-
+  let end = nums[nums.length];
   console.log(nums)
-  for(let i=0; i<nums.length; i++) {
-    map.set(nums[i], i);
+
+  if(nums[0] != 0) {
+    return 0;
   }
 
-  for(let i=nums[0]; i < nums[nums.length-1]; i++) {
-    if(map.has(i)) {
-      continue;
-    }
-    return i;
+  for(let i=0; i < end; i++) {
+    map.set(nums[i],i);
   }
-};
+
+  console.log(map)
+
+  for(let i=0; i < end; i++) {
+    if(map.has(i)) {
+      console.log("The map has the element " + i + "and at the same index the nums array has " + nums[i]);
+      continue;
+    } else if(!map.has(i)) {
+      return i;
+    }
+    }
+  return end + 1;
+  };
 
 console.log(missingNumber([1,0]));
